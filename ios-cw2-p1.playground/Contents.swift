44203192d9650d0c8c03e9b7d621235ecdf9f429
jumana-alshameri <66526830+jumana-alshameri@users.noreply.github.com>
2020-06-17 20:40:31 +0300

    done

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,15 @@
+let family = ["jumana", "shehab", "aya", "ali", "mohamd"]
+
+var evens = [2, 6, 8, 10,  12]
+
+var grades  = [90.8, 90.5,90,0]
+
+var trueFalse = [true, true,true,false]
+
+print (family[0], family[4])
+
+print (evens[0],  evens[4])
+
+print (grades[0], grades[3])
+
+
---
+++ ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,10 @@
+var fruits =  ["mango", "orange", "potato","peach"]
+print (fruits[0])
+print (fruits[3])
+
+fruits.append("strawberry")
+print (fruits)
+fruits.remove(at: 2)
+print (fruits)
+fruits += ["banana", "watermelon", "pinapple",  "kiwi"]
+fruits.removeAll()
